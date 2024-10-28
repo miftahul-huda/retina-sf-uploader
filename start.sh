a1e8f1c6a8d70ae9e36687a873f1f50e153962e5
M. Huda <mhuda@users-MacBook-Pro.local>
2024-10-28 14:09:20 +0700

    addd environment

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#export GOOGLE_APPLICATION_CREDENTIALS=/Users/mhuda/Works/Credentials/telkomsel-retail-intelligence-sa.json
-export GOOGLE_APPLICATION_CREDENTIALS=/media/sf_Works/Credentials/telkomsel-retail-intelligence-sa.json 
+export GOOGLE_APPLICATION_CREDENTIALS=/Users/mhuda/Works/Credentials/telkomsel-retail-intelligence-sa.json
+#export GOOGLE_APPLICATION_CREDENTIALS=/media/sf_Works/Credentials/telkomsel-retail-intelligence-sa.json 
 node app.js
